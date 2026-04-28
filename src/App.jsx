84390de430577a7b9010389ccce757e67e1e0faf
Arthur <jose.arthur.senai404@gmail.com>
2026-04-28 16:25:05 -0300

    novo botao de favoritar

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,4 @@
+import { useState } from "react";
 import { Sidebar } from "./components/Sidebar";
 import { Header } from "./components/Header";
 import { gamesData } from "./data/games";
@@ -5,23 +6,56 @@ import { GameCard } from "./components/GameCard";
 import "./App.css";
 
 function App() {
+  const [search, setSearch] = useState('');
+  const [activeTab, setActiveTab] = useState("dash");
+  const [favorites, setFavorites] = useState([]); 
+
+  const filteredGames = gamesData
+  .filter((game) => activeTab === "dash" || favorites.includes(game.id))
+  .filter((game) => game.title.toLowerCase().includes(search.toLowerCase()));
+
+
+const toggleFavorite = (id) => {
+// verifica se o item esta no array, se sim ele substitui o item e o reorganiza
+  setFavorites((prev) => prev.includes(id) ? prev.filter((favId) => favId !== id) : [...prev, id],
+
+  );
+
+}
+
   return (
     <div className="vortex-app">
-      <Sidebar />
+      <Sidebar activeTab={activeTab} setActiveTab={setActiveTab} />
 
       <main className="vortex-main">
-        <Header />
+
+        <Header search = {search} setSearch={setSearch} />
+
         <div className="vortex-content">
-          <h2 className="section-title">Jogos Recentes</h2>
+
+          <h2 className="section-title">
+            {activeTab === "dash" && "Dashboard"}
+            {activeTab === "favorite" && "meus jogos"}
+            {activeTab === "profile" && "Perfil"}
+          </h2>
+
           <div className="vortex-grid">
-            {gamesData.map((g) => (
-              <GameCard
+
+           {filteredGames.length > 0 ? (
+            filteredGames.map((g) => (
+               <GameCard
                 key={g.id}
                 title={g.title}
                 category={g.category}
                 banner={g.banner}
+                isFavorite={favorites.includes(g.id)}
+                onFavorite={() => toggleFavorite(g.id)}
               />
-            ))}
+            ))
+           ) : (
+            <p style={{color: "#94a3b8"}}>Nenhum jogo encontrado...</p>
+           )}
+            
           </div>
         </div>
       </main>
--- src/components/GameCard.jsx
+++ src/components/GameCard.jsx
@@ -1,10 +1,23 @@
 import "./GameCard.css";
-import { Play } from "lucide-react";
+import { Play, Heart } from "lucide-react";
 
-export function GameCard({ title, category, banner }) {
+
+export function GameCard({ title, category, banner, isFavorite, onFavorite }) {
   return (
     <div className="vortex-card">
       <img src={banner} alt={title} className="card-img" />
+
+      <button
+        className={`favorite-btn ${isFavorite ? "active" : ''}`}
+        onClick={onFavorite}
+      >
+        <Heart
+          size={20}
+          fill={isFavorite ? "#7c3aed" : "tranparent"}
+          stroke={isFavorite ? "#7c3aed" : "white"}
+        />
+
+      </button>
 
       <div className="card-info">
         <h4>{title}</h4>
